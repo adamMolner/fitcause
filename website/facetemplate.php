5e6262f10e1036d206e0e4a16d50cc0b4ce6a356
Alex <ajmanor@wayne.edu>
2018-06-24 02:15:41 -0400

    Formatting face page

--- website/facetemplate.php
+++ website/facetemplate.php
@@ -1,30 +1,24 @@
 <?php
-
 require 'inc/header.php';
+require 'connect_file.php';
 
-include 'connect_file.php';
+// DB Info
+// id, name, imageLink, description
 
 $id = $_GET["id"];
 $result = mysqli_query($con,"SELECT * FROM face WHERE id='$id'");
 
+while($row = mysqli_fetch_array($result)) {
+?>
 
-
-while($row = mysqli_fetch_array($result))
-  {
-  echo'<div class = "container">';
-
-  echo '<div>';
-  echo $row['name'];
-  echo '</div>';
-  echo '<img src="'.$row['imageLink'].'">';
-  
-  echo '<p>' .$row['description']. '</p>';
-  
-  
-  
-  echo '</div>';
-}
-
+<div class="container">
+	<div class="row">
+		<h1><?php echo $row['name'];?></h1>
+		<img src="<?php echo $row['imageLink'];?>" alt="<?php echo $row['description']?>">
+		<p><?php echo $row['description'];?></p>
+	</div> <!-- End of row -->
+</div> <!-- End of container -->
+<?php } 
 ?>
 
 
