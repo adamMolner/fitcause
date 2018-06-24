2a15f211320e340ab78414547aa363e57de0a884
osman.ghoni1 <osman.ghoni1@gmail.com>
2018-06-24 00:40:18 -0400

    created faceTempalte page

--- website/facetemplate.php
+++ website/facetemplate.php
@@ -4,23 +4,26 @@ require 'inc/header.php';
 
 include 'connect_file.php';
 
-
-$result = mysqli_query($con,"SELECT * FROM face WHERE id=1");
+$id = $_GET["id"];
+$result = mysqli_query($con,"SELECT * FROM face WHERE id='$id'");
 
 
 
 while($row = mysqli_fetch_array($result))
   {
+  echo'<div class = "container">';
 
+  echo '<div>';
   echo $row['name'];
-echo '<img src="'.$row['imageLink'].'">';
-echo '<p>' .$row['description']. '</p>';
+  echo '</div>';
+  echo '<img src="'.$row['imageLink'].'">';
+  
+  echo '<p>' .$row['description']. '</p>';
+  
+  
+  
+  echo '</div>';
 }
-
-
-
-
-
 
 ?>
 
--- website/index.php
+++ website/index.php
@@ -10,7 +10,7 @@
             <h2 class="brandName">American Brain Tumor Association</h2>
             <p class="brandDescription">The American Brain Tumor Association is the only national nonprofit organization in the United States dedicated to providing support services and programs to brain tumor patients and their families, as well as the funding of brain tumor research.</p>
             <div class="btn-group">
-                <a href="query.php?id=1"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
+                <a href="facetemplate.php?id=1"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
             </div>
         </div>
         <div class="col-md brand">
@@ -18,15 +18,15 @@
             <h2 class="brandName">ALS Association</h2>
             <p class="brandDescription">The ALS Association is the only national non-profit organization fighting Lou Gehrig’s Disease on every front.</p>
             <div class="btn-group">
-                <a href="http://www.alsa.org" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
+                <a href="facetemplate.php?id=2" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
             </div>
         </div>
         <div class="col-md brand">
             <img src="img/codeorg.png" alt="Code.org" class="brandThumbnail">
             <h2 class="brandName">Code.org</h2>
-            <p class="brandDescription">The American Brain Tumor Association is the only national nonprofit organization in the United States dedicated to providing support services and programs to brain tumor patients and their families, as well as the funding of brain tumor research.</p>
+            <p class="brandDescription">Code.org is a non-profit organization that aims to encourage people, particularly school students in the United States, to learn computer science.</p>
             <div class="btn-group">
-                <a href="http://www.code.org" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a>
+                <a href="facetemplate.php?id=3" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a>
             </div>
         </div>
     </div> <!-- End of row-->
@@ -37,7 +37,7 @@
                 <h2 class="brandName">Autism Speaks</h2>
                 <p class="brandDescription">Autism Speaks is dedicated to promoting solutions, across the spectrum and throughout the life span, for the needs of individuals with autism and their families through advocacy and support; increasing understanding and acceptance of people with autism spectrum disorder; and advancing research into causes and better interventions for autism spectrum disorder and related conditions.</p>
                 <div class="btn-group">
-                    <a href="http://www.autismspeaks.org" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
+                    <a href="facetemplate.php?id=4" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
                 </div>
             </div>
             <div class="col-md brand">
@@ -45,7 +45,7 @@
                 <h2 class="brandName">Detroit Zoological Society</h2>
                 <p class="brandDescription">The Detroit Zoological Society is a renowned leader in humane education, wildlife conservation, animal welfare and environmental sustainability.</p>
                 <div class="btn-group">
-                    <a href="http://www.detroitzoo.com" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
+                    <a href="facetemplate.php?id=5" target="_blank"><button type="button" class="btn btn-sm btn-outline-secondary">Learn More</button></a> 
                 </div>
             </div>
             <div class="col-md brand">
